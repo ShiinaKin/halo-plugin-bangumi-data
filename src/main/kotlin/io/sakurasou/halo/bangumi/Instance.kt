0a313fc499cb8dc191d67fc60e524486b859deab
Shiina Kin <shiina@sakurasou.io>
2025-07-27 23:43:47 +0800

    feat: add scheduling and daily task - enable scheduling with @EnableScheduling annotation - add ThreadPoolTaskScheduler bean for task management - implement scheduled daily task to update user data - log execution of daily task using KotlinLogging

--- src/main/kotlin/io/sakurasou/halo/bangumi/Instance.kt
+++ src/main/kotlin/io/sakurasou/halo/bangumi/Instance.kt
@@ -1,5 +1,6 @@
 package io.sakurasou.halo.bangumi
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.cache.HttpCache
@@ -8,16 +9,29 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.header
 import io.ktor.serialization.kotlinx.json.json
 import io.sakurasou.halo.bangumi.api.DefaultApi
+import io.sakurasou.halo.bangumi.service.BangumiService
 import kotlinx.serialization.json.Json
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+import reactor.core.scheduler.Schedulers
 
 /**
  * @author Shiina Kin
  * 2025/7/21 15:01
  */
+@EnableScheduling
 @Configuration
 open class Instance {
+    private val logger = KotlinLogging.logger {}
+
+    @Autowired
+    private lateinit var context: ApplicationContext
+
     @Bean
     open fun json() =
         Json {
@@ -45,4 +59,22 @@ open class Instance {
                 }
             },
         )
+
+    @Bean
+    open fun scheduledTask() =
+        ThreadPoolTaskScheduler().apply {
+            poolSize = 2
+            setThreadNamePrefix("bangumi-scheduler-")
+            initialize()
+        }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    open fun dailyTask() {
+        val bangumiService = context.getBean(BangumiService::class.java)
+        bangumiService
+            .updateUserData()
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe()
+        logger.info { "Daily task executed: User data updated." }
+    }
 }
